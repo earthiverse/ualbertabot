cd4ac9be7220751c881cfe7b25d3906aababce2b
TianZhiWang <wtianzhi@gmail.com>
2015-11-29 12:44:46 -0700

    Overlords run from threats, and scout more bases

--- UAlbertaBot/Source/OverlordManager.cpp
+++ UAlbertaBot/Source/OverlordManager.cpp
@@ -1,6 +1,7 @@
 #include "OverlordManager.h"
 #include "Common.h"
 #include <random>
+#include "UnitUtil.h"
 
 using namespace UAlbertaBot;
 
@@ -13,52 +14,75 @@ OverlordManager::OverlordManager()
 void OverlordManager::update(const BWAPI::Unitset & overlords)
 {
 	for (auto & overlord : overlords) {
-		
-		if (!scouting)
-		{
-			_mainOverlord = overlord;
-			_overlordScouts.insert(overlord);
-			scouting = true;
-		}
-		
-		if (!_mainOverlord->isMoving())
-		{
-			BWTA::BaseLocation * enemyBaseLocation = InformationManager::Instance().getMainBaseLocation(BWAPI::Broodwar->enemy());
 
-			if (!enemyBaseLocation) //if not found yet
-			{
-				for (BWTA::BaseLocation * startLocation : BWTA::getStartLocations())
-				{
-					// if we haven't explored it yet
-					if (!BWAPI::Broodwar->isExplored(startLocation->getTilePosition()))
-					{
-						_mainOverlord->move(BWAPI::Position(startLocation->getTilePosition()));
-					}
-				}
-			}
-			
-			if (enemyBaseLocation && !BWAPI::Broodwar->isExplored(enemyBaseLocation->getTilePosition()))
-			{
-				_mainOverlord->move(BWAPI::Position(enemyBaseLocation->getTilePosition()));
-			}
+		if (_overlordScouts.size() == 0)
+		{
+			_startLocations = BWTA::getStartLocations();
+			for (auto i : BWTA::getStartLocations())
+				_startStack.push(BWAPI::Position(i->getTilePosition()));
+			for (auto i : BWTA::getBaseLocations())
+				if (_startLocations.find(i) == _startLocations.end())
+					_locationsStack.push(BWAPI::Position(i->getTilePosition()));
+				
+			_mainOverlord = overlord;
+			scouting = true;
 		}
 
 		if (!_overlordScouts.contains(overlord))
 		{
-			moveRandomly(overlord);
+			if (_startStack.size() > 0)
+			{
+				BWAPI::Position pos = _startStack.top();
+				_startStack.pop();
+				overlord->move(pos);
+			}
+			else if (_startLocations.size() > 0)
+			{
+				BWAPI::Position pos = _locationsStack.top();
+				_locationsStack.pop();
+				overlord->move(pos);
+			} 
+			else
+			{
+				moveRandomly(overlord);
+			}
 			_overlordScouts.insert(overlord);
 		}
 	}
-}
 
+	for (auto overlord : _overlordScouts)
+	{
+		if (!overlord || !overlord->exists() || !(overlord->getHitPoints() > 0))
+		{
+			continue;
+		}
+		BWAPI::Unitset enemyNear;
+		MapGrid::Instance().GetUnits(enemyNear, overlord->getPosition(), 400, false, true);
+		for (auto unit : enemyNear)
+		{
+			if (UnitUtil::CanAttackAir(unit))
+			{
+				BWAPI::Position fleePosition(overlord->getPosition() - unit->getPosition() +overlord->getPosition());
+				BWAPI::Broodwar->drawLineMap(overlord->getPosition(), fleePosition, BWAPI::Colors::Cyan);
+				overlord->move(fleePosition);
+			}
+		}
+		if (!overlord->isMoving())
+		{
+			moveRandomly(overlord);
+		}
+	}
+}
 void OverlordManager::moveRandomly(const BWAPI::Unit & overlord)
 {
 	static std::random_device rd;
 	static std::mt19937 gen(rd());
-	static std::uniform_int_distribution<int> randWidth = std::uniform_int_distribution<int>(0, BWAPI::Broodwar->mapWidth() * 64);
-	static std::uniform_int_distribution<int> randHeight = std::uniform_int_distribution<int>(0, BWAPI::Broodwar->mapHeight() * 64);
+	static std::uniform_int_distribution<int> randWidth = std::uniform_int_distribution<int>(0, BWAPI::Broodwar->mapWidth()*32);
+	static std::uniform_int_distribution<int> randHeight = std::uniform_int_distribution<int>(0, BWAPI::Broodwar->mapHeight()*32);
 	int width = randWidth(gen);
 	int height = randHeight(gen);
+	//int w = rand() % BWAPI::Broodwar->mapWidth()*32;
+	//int h = rand() % BWAPI::Broodwar->mapHeight()*32;
 
 	BWAPI::Position pos = BWAPI::Position(width, height);
 	overlord->move(pos);
--- UAlbertaBot/Source/OverlordManager.h
+++ UAlbertaBot/Source/OverlordManager.h
@@ -2,15 +2,22 @@
 
 #include "Common.h"
 #include "MicroManager.h"
+#include <stack>
 
 namespace UAlbertaBot
 {
 class OverlordManager
 {	
 	BWAPI::Unitset _overlordScouts;
+	std::map<BWAPI::Unit, BWAPI::Position> _overlordpos;
 	bool scouting = false;
 	BWAPI::Unit _mainOverlord;
-	
+	std::vector<BWAPI::Position> _possibleBases;
+	std::set < BWTA::BaseLocation *,
+		std::less<BWTA::BaseLocation *>,
+		std::allocator < BWTA::BaseLocation * >> _startLocations;
+	std::stack<BWAPI::Position> _startStack;
+	std::stack<BWAPI::Position> _locationsStack;
 	void moveRandomly(const BWAPI::Unit & overlord);
 
 public:
