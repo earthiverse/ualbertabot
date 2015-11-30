daa0c24253880461bda6f3bac00e4a93617eed93
Carson McLean <camclean@ualberta.ca>
2015-11-29 19:23:44 -0700

    Fixes to crashes b/c of Zerglings

--- UAlbertaBot/Source/ZerglingManager.cpp
+++ UAlbertaBot/Source/ZerglingManager.cpp
@@ -15,16 +15,25 @@ void ZerglingManager::executeMicro(const BWAPI::Unitset & targets)
 	BWAPI::Unitset zerglingTargets = targets;
 
 	for (auto & zerg : zerglings) {
+		if (!zerg || !zerg->exists() || !(zerg->getHitPoints() > 0)) {
+			continue;
+		}
+
 		//BWAPI::Broodwar->printf("Works");
 		//TODO: Check order type? Attack or Defend?
 
 		if (zerglingUnitShouldRetreat(zerg, targets)) {
 			BWAPI::Broodwar->printf("Should retreat");
 			// TODO : Flee from closest enemy
-			BWAPI::Unit closest = zerg->getClosestUnit(BWAPI::Filter::IsEnemy);
+			//BWAPI::Unit closest = zerg->getClosestUnit(BWAPI::Filter::IsEnemy);
+			BWAPI::Unit target = getTarget(zerg, targets);
+			if (!target || !target->exists() || !(target->getHitPoints() > 0)) {
+				continue;
+			}
+
 			// Technique of retreat taken from Micro.cpp's SmartKiteTarget.
 			try {
-				BWAPI::Position fleeTo(zerg->getPosition() - closest->getPosition() + zerg->getPosition());
+				BWAPI::Position fleeTo(zerg->getPosition() - target->getPosition() + zerg->getPosition());
 				BWAPI::Broodwar->drawLineMap(zerg->getPosition(), fleeTo, BWAPI::Colors::Cyan);
 				Micro::SmartMove(zerg, fleeTo);
 			}
@@ -64,26 +73,86 @@ bool ZerglingManager::zerglingUnitShouldRetreat(BWAPI::Unit attacker, const BWAP
 }
 
 BWAPI::Unit ZerglingManager::getTarget(BWAPI::Unit attacker, const BWAPI::Unitset & targets) {
-	//int highPriority = 0;
+	int highPriority = 0;
 	double closestDist = std::numeric_limits<double>::infinity();
 	BWAPI::Unit closestTarget = nullptr;
 
 	// for each target possiblity
 	for (auto & unit : targets)
 	{
-		//int priority = getAttackPriority(meleeUnit, unit);
+		int priority = getAttackPriority(attacker, unit);
 		int distance = attacker->getDistance(unit);
 
 		// if it's a higher priority, or it's closer, set it
-		//if (!closestTarget || (priority > highPriority) || (priority == highPriority && distance < closestDist))
-		if (closestDist > distance)
+		if (!closestTarget || (priority > highPriority) || (priority == highPriority && distance < closestDist))
 		{
 			closestDist = distance;
-			//highPriority = priority;
+			highPriority = priority;
 			closestTarget = unit;
 		}
 	}
 
 	return closestTarget;
 }
+
+int ZerglingManager::getAttackPriority(BWAPI::Unit attacker, BWAPI::Unit unit) {
+	BWAPI::UnitType type = unit->getType();
+
+	if (attacker->getType() == BWAPI::UnitTypes::Protoss_Dark_Templar
+		&& unit->getType() == BWAPI::UnitTypes::Terran_Missile_Turret
+		&& (BWAPI::Broodwar->self()->deadUnitCount(BWAPI::UnitTypes::Protoss_Dark_Templar) == 0))
+	{
+		return 13;
+	}
+
+	if (attacker->getType() == BWAPI::UnitTypes::Protoss_Dark_Templar && unit->getType().isWorker())
+	{
+		return 12;
+	}
+
+	// highest priority is something that can attack us or aid in combat
+	if (type == BWAPI::UnitTypes::Terran_Bunker)
+	{
+		return 11;
+	}
+	else if (type == BWAPI::UnitTypes::Terran_Medic ||
+		(type.groundWeapon() != BWAPI::WeaponTypes::None && !type.isWorker()) ||
+		type == BWAPI::UnitTypes::Terran_Bunker ||
+		type == BWAPI::UnitTypes::Protoss_High_Templar ||
+		type == BWAPI::UnitTypes::Protoss_Reaver ||
+		(type.isWorker() && unitNearChokepoint(unit)))
+	{
+		return 10;
+	}
+	// next priority is worker
+	else if (type.isWorker())
+	{
+		return 9;
+	}
+	// next is special buildings
+	else if (type == BWAPI::UnitTypes::Zerg_Spawning_Pool)
+	{
+		return 5;
+	}
+	// next is special buildings
+	else if (type == BWAPI::UnitTypes::Protoss_Pylon)
+	{
+		return 5;
+	}
+	// next is buildings that cost gas
+	else if (type.gasPrice() > 0)
+	{
+		return 4;
+	}
+	else if (type.mineralPrice() > 0)
+	{
+		return 3;
+	}
+	// then everything else
+	else
+	{
+		return 1;
+	}
+}
+
 
--- UAlbertaBot/Source/ZerglingManager.h
+++ UAlbertaBot/Source/ZerglingManager.h
@@ -10,10 +10,12 @@ class ZerglingManager : public MicroManager
 public:
 
 	ZerglingManager();
+	~ZerglingManager() {} 
 	void executeMicro(const BWAPI::Unitset & targets);
 
 	bool zerglingUnitShouldRetreat(BWAPI::Unit attacker, const BWAPI::Unitset & targets);
 	BWAPI::Unit getTarget(BWAPI::Unit attacker, const BWAPI::Unitset & targets);
+	int getAttackPriority(BWAPI::Unit attacker, BWAPI::Unit unit);
 };
 
 
